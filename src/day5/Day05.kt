27219b760a40701b616994352c82c35fa81752b3
Damian Kruszynski <damian.kruszynski-extern@dbschenker.com>
2023-12-07 11:22:45 +0100

    day 5 2/2 - that was hard

--- src/day5/Day05.kt
+++ src/day5/Day05.kt
@@ -1,6 +1,11 @@
 package day5
 
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.runBlocking
 import println
 import readInput
 
@@ -23,18 +28,27 @@ fun main() {
     }
 
     fun getValueFor(almanac: Almanac, lookingFor: Long, recipeType: RecipeType): Long {
-        val list = almanac.conversionRecipes.filter { it.type == recipeType }.map {
-            val sourceRange = (it.sourceRangeStart..<it.sourceRangeStart + it.rangeLength)
-            val indexOf = (sourceRange).indexOf(lookingFor)
-            println("Recipe Type:$recipeType looking for $lookingFor on index: $indexOf")
-            if (indexOf == -1) {
-                lookingFor
-            } else {
-                (it.destinationRangeStart..<it.destinationRangeStart + it.rangeLength).elementAtOrElse(indexOf) { lookingFor }
-
-            }
+        val firstTraffic = almanac.conversionRecipes.firstOrNull {
+            it.type == recipeType && (it.sourceRangeStart..<it.sourceRangeStart + it.rangeLength).contains(lookingFor)
         }
-        return list.find { it != lookingFor } ?: list.first()
+        return if (firstTraffic != null) {
+            val indexOf = lookingFor - firstTraffic.sourceRangeStart
+            firstTraffic.destinationRangeStart + indexOf
+        } else
+            lookingFor
+    }
+
+    fun getValueForReverse(almanac: Almanac, lookingFor: Long, recipeType: RecipeType): Long {
+        val firstTraffic = almanac.conversionRecipes.firstOrNull {
+            it.type == recipeType && (it.destinationRangeStart..<it.destinationRangeStart + it.rangeLength).contains(
+                lookingFor
+            )
+        }
+        return if (firstTraffic != null) {
+            val indexOf = lookingFor - firstTraffic.destinationRangeStart
+            firstTraffic.sourceRangeStart + indexOf
+        } else
+            lookingFor
     }
 
     fun part1(input: List<String>): Long {
@@ -67,8 +81,86 @@ fun main() {
     }
 
     fun part2(input: List<String>): Long {
-        return 0
+        val almanac = mapToAlmanac(input)
+        val seedsRange =
+            almanac.seeds.mapIndexed { index, it -> if (index % 2 == 0) it..<it + almanac.seeds[index + 1] else null }
+                .filterNotNull()
+        return runBlocking(Dispatchers.IO) {
+            coroutineScope {
+                seedsRange.map { seed ->
+                    async {
+                        coroutineScope {
+                            seed.minOf {
+                                val seedToSoil = getValueFor(almanac, it, RecipeType.SEED_TO_SOIL)
+                                val soilToFertilizer = getValueFor(almanac, seedToSoil, RecipeType.SOIL_TO_FERTILIZER)
+                                val fertilizerToWater =
+                                    getValueFor(almanac, soilToFertilizer, RecipeType.FERTILIZER_TO_WATER)
+                                val waterToLight = getValueFor(almanac, fertilizerToWater, RecipeType.WATER_TO_LIGHT)
+                                val lightToTemperature =
+                                    getValueFor(almanac, waterToLight, RecipeType.LIGHT_TO_TEMPERATURE)
+                                val temperatureToHumidity =
+                                    getValueFor(almanac, lightToTemperature, RecipeType.TEMPERATURE_TO_HUMIDITY)
+                                val humidityToLocation =
+                                    getValueFor(almanac, temperatureToHumidity, RecipeType.HUMIDITY_TO_LOCATION)
+                                println(
+                                    listOf(
+                                        seedToSoil,
+                                        soilToFertilizer,
+                                        fertilizerToWater,
+                                        waterToLight,
+                                        lightToTemperature,
+                                        temperatureToHumidity,
+                                        humidityToLocation
+                                    )
+                                )
+                                humidityToLocation
+                            }
+                        }
+                    }
+                }.awaitAll().minOf { it }
+            }
+        }
     }
+
+    fun part2Reverse(input: List<String>): Long {
+        val almanac = mapToAlmanac(input)
+        val seedsRange =
+            almanac.seeds.mapIndexed { index, it -> if (index % 2 == 0) it..<it + almanac.seeds[index + 1] else null }
+                .filterNotNull()
+        val locationListRange = almanac.conversionRecipes.filter { it.type == RecipeType.HUMIDITY_TO_LOCATION }
+            .map { it.destinationRangeStart..<it.destinationRangeStart + it.rangeLength }
+            .sortedBy { it.first }
+        var lowestLocation = Long.MAX_VALUE
+        locationListRange.find { locationRange ->
+            locationRange.first { location ->
+                val humidity = getValueForReverse(almanac, location, RecipeType.HUMIDITY_TO_LOCATION)
+                val temperature = getValueForReverse(almanac, humidity, RecipeType.TEMPERATURE_TO_HUMIDITY)
+                val light = getValueForReverse(almanac, temperature, RecipeType.LIGHT_TO_TEMPERATURE)
+                val water = getValueForReverse(almanac, light, RecipeType.WATER_TO_LIGHT)
+                val fertilizer = getValueForReverse(almanac, water, RecipeType.FERTILIZER_TO_WATER)
+                val soil = getValueForReverse(almanac, fertilizer, RecipeType.SOIL_TO_FERTILIZER)
+                val seed = getValueForReverse(almanac, soil, RecipeType.SEED_TO_SOIL)
+                println(
+                    "For Location: $location ${
+                        listOf(
+                            location,
+                            humidity,
+                            temperature,
+                            light,
+                            water,
+                            fertilizer,
+                            soil,
+                            seed
+                        )
+                    }"
+                )
+                lowestLocation = location
+                seedsRange.any { it.contains(seed) }
+            } != 0L
+        }
+        return lowestLocation
+    }
+
 
     val testInput = readInput("Day05_test")
     part1(testInput).println()
@@ -78,11 +170,12 @@ fun main() {
     val input = readInput("Day05")
     part1(input).println()
     // check(part1(input) == 23847)
-    /*
-        val testInput2 = readInput("Day04_test2")
-        check(part2(testInput2) == 30)
+
+    val testInput2 = readInput("Day05_test2")
+    part2(testInput2).println()
+    check(part2(testInput2) == 46L)
 
 
-        part2(input).prLongln()
-        check(part2(input) == 8570000)*/
+    part2Reverse(input).println()
+    //check(part2(input) == 6472060)
 }
