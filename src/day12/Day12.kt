b701fa7eff85e51c7cd38f9200d23e1e53275311
Damian Kruszynski <damian.kruszynski-extern@dbschenker.com>
2023-12-11 10:51:57 +0100

    day 11 start

---
+++ src/day12/Day12.kt
@@ -0,0 +1,26 @@
+package day1
+
+import readInput
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        return 0
+    }
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day11_test")
+    check(part1(testInput) == 142)
+
+   // val input = readInput("Day01")
+  //  part1(input).println()
+
+   // val testInput2 = readInput("Day01_test2")
+   // check(part2(testInput2) == 281)
+
+   // part2(input).println()
+}
